948627bdc262d4debefc26091197d54b88a99f74
kylemccann <kylemcc08@gmail.com>
2018-02-13 19:43:10 +0000

    Update Config.java

--- prototype/src/main/java/proto/Microsoft/Config.java
+++ prototype/src/main/java/proto/Microsoft/Config.java
@@ -8,8 +8,8 @@ package proto.Microsoft;
 
 public class Config {
     //These are the omly variables which need to be changed for your configuration.̵
-    private String ApiKey1 = "43fca961c43b4ea6b4a60ca779eb20e9";
-    private String ApiKey2 = "ca6bd76c6bd740e5b9c2859ef6bf5395";
+    private String ApiKey1 = "";
+    private String ApiKey2 = "";
     private String endPointUrl = "https://api.cognitive.microsoft.com/sts/v1.0";
     private String language = "en-GB";
     private String recognitionMode = "interactive"; //Utterances of 2-3 seconds
